27961e85388925330bf7d3f0a717f10509eb47fe
Vít Smolík <me@smoliicek.cz>
2026-01-28 22:43:27 +0100

    basic auth works
    Signed-off-by: Vít Smolík <me@smoliicek.cz>

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"smoliicek/pufferstarter/pkg/auth"
+
+	"github.com/spf13/cobra"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	var serverID string
+	var status string
+
+	var rootCmd = &cobra.Command{
+		Use:   "pufferstarter-cli",
+		Short: "A CLI tool for server management",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			if cmd.Flags().Changed("listAll") {
+				fmt.Println("Listing all servers...")
+				// getAllServers(0)
+				return
+			}
+
+			if cmd.Flags().Changed("getInfo") {
+				if serverID == "" {
+					fmt.Println("You need to specify --id to run this function.")
+					os.Exit(1)
+				}
+				fmt.Printf("Getting info for server %s\n", serverID)
+				// getServerInfo(ip, token, 0, serverID)
+				return
+			}
+
+			if cmd.Flags().Changed("setStatus") {
+				if serverID == "" {
+					fmt.Println("You need to specify --id to run this function.")
+					os.Exit(1)
+				}
+
+				switch status {
+				case "on":
+					fmt.Println("Starting server", serverID)
+					// setServerStatus(ip, token, 0, serverID, "on")
+				case "off":
+					fmt.Println("Stopping server", serverID)
+					// setServerStatus(ip, token, 0, serverID, "off")
+				case "kill":
+					fmt.Println("Killing server", serverID)
+					// setServerStatus(ip, token, 0, serverID, "kill")
+				default:
+					fmt.Println("Invalid status. Use on, off, or kill.")
+				}
+				return
+			}
+
+			if serverID != "" {
+				fmt.Println("You can't run --id by itself.")
+			}
+			cmd.Help()
+		},
+	}
+
+	rootCmd.Flags().StringVar(&serverID, "id", "", "Set the Server ID (length: 8)")
+	rootCmd.Flags().BoolP("listAll", "l", false, "Lists all servers and IDs")
+	rootCmd.Flags().BoolP("getInfo", "g", false, "Gets info about a server (requires --id)")
+	rootCmd.Flags().StringVarP(&status, "setStatus", "s", "", "Set status: on, off, kill (requires --id)")
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func getToken() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	serverIP := os.Getenv("SERVER_IP")
+
+	token, err := auth.GetAuthToken(clientID, clientSecret, serverIP)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Auth Token:", token)
+}
---
+++ pkg/auth/auth.go
@@ -0,0 +1,60 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func GetAuthToken(clientID, clientSecret, serverIP string) (string, error) {
+
+	grantType := "client_credentials"
+
+	data := url.Values{}
+	data.Set("client_id", clientID)
+	data.Set("client_secret", clientSecret)
+	data.Set("grant_type", grantType)
+
+	bodyReader := strings.NewReader(data.Encode())
+
+	req, err := http.NewRequest("POST", "https://"+serverIP+"/oauth2/token", bodyReader)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("User-Agent", "pufferstarter-cli/0.1")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("token request failed: %s", resp.Status)
+	}
+
+	var tr struct {
+		AccessToken string `json:"access_token"`
+	}
+	if err := json.Unmarshal(body, &tr); err != nil {
+		return "", err
+	}
+
+	if tr.AccessToken == "" {
+		return "", fmt.Errorf("access_token not found in response")
+	}
+
+	return tr.AccessToken, nil
+}
